e5c1ebb1ef6df92307c3f633a50764a904a13554
Aman <131179388+amad3us47@users.noreply.github.com>
2023-12-12 23:58:59 +0530

    Update certsh.py

--- certsh.py
+++ certsh.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import re
 
 main_url = "https://crt.sh:443/?q=flipkart.com"
-main_headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/117.0", "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8", "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate", "Referer": "https://crt.sh/", "Upgrade-Insecure-Requests": "1", "Sec-Fetch-Dest": "document", "Sec-Fetch-Mode": "navigate", "Sec-Fetch-Site": "same-origin", "Sec-Fetch-User": "?1", "Te": "trailers", "Connection": "close"}
+main_headers = {"Content-Type":"text"}
 r=requests.get(main_url, headers=main_headers)
 soup=BeautifulSoup(r.content,'html5lib')
 urls=[]
