caa816f0ebe0c94cf75771d61fac0fba3931e326
Aman <131179388+amad3us47@users.noreply.github.com>
2023-12-13 00:05:37 +0530

    Update maxdomains.py

--- maxdomains.py
+++ maxdomains.py
@@ -2,12 +2,12 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
-main_url1 = "https://crt.sh:443/?q=flipkart.com"}
+main_url1 = "https://crt.sh:443/?q=flipkart.com"
 main_headers1 = {"Content-Type":"text"}
 r1=requests.get(main_url1, headers=main_headers1)
 soup1=BeautifulSoup(r1.content,'html5lib')
 urls1=[]
-for links in soup1.find_all(text=re.compile("flipkart.com")):
+for links in soup1.find_all(string=re.compile("flipkart.com")):
     if(links[0]!="*"):
         print(links)
 main_url2 = "https://subdomainfinder.c99.nl/scans/2023-12-11/flipkart.com"
@@ -15,7 +15,7 @@ main_headers2 = {"Content-Type":"text"}
 r2=requests.get(main_url2, headers=main_headers2)
 soup2=BeautifulSoup(r2.content,'html5lib')
 urls2=[]
-for links in soup2.find_all(text=re.compile("flipkart.com")):
+for links in soup2.find_all(string=re.compile("flipkart.com")):
     if(links!="More scans of flipkart.com"):
         print(links)
     else:
